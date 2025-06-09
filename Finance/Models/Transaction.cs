fd8f7cb254d334fdd7947dcd1bc9d45cc7af68f3
Fabricyo Barreto <bass.barreto@gmail.com>
2025-06-09 20:16:57 -0300

    feat: translate column Type in Datagrid

--- Finance/Models/Transaction.cs
+++ Finance/Models/Transaction.cs
@@ -7,6 +7,19 @@ public class Transaction
     public decimal Value { get; set; }
     public DateTime Date { get; set; }
     public TypeTransaction Type { get; set; }
+
+    public string TypeTransactionFormatted
+    {
+        get
+        {
+            return Type switch
+            {
+                TypeTransaction.Income => "Entrada",
+                TypeTransaction.Outcome => "Saída",
+                _ => "Desconhecido"
+            };        
+        }
+    }
 }
 
 public enum TypeTransaction
