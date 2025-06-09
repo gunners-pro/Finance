25881f2a8b68c6a84bd3f40f3a39125f66462140
Fabricyo Barreto <bass.barreto@gmail.com>
2025-06-09 20:03:27 -0300

    feat: add calcs to balance, incomes and outcomes

--- Finance/App.xaml.cs
+++ Finance/App.xaml.cs
@@ -1,14 +1,26 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using System.Globalization;
 using System.Windows;
 
-namespace Finance
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+namespace Finance;
 
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        CultureInfo culture = new("pt-BR");
+        Thread.CurrentThread.CurrentCulture = culture;
+        Thread.CurrentThread.CurrentUICulture = culture;
+
+        FrameworkElement.LanguageProperty.OverrideMetadata(
+            typeof(FrameworkElement),
+            new FrameworkPropertyMetadata(
+                System.Windows.Markup.XmlLanguage.GetLanguage(culture.IetfLanguageTag)
+                ));
+
+        base.OnStartup(e);
+    }
 }
--- Finance/ViewModels/TransactionsViewModel.cs
+++ Finance/ViewModels/TransactionsViewModel.cs
@@ -1,17 +1,49 @@
 ﻿using Finance.Data;
 using Finance.Models;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Finance.ViewModels;
 
-public class TransactionsViewModel
+public class TransactionsViewModel : INotifyPropertyChanged
 {
+    public event PropertyChangedEventHandler? PropertyChanged;
     public ObservableCollection<Transaction> TransactionsList { get; set; }
+
+    private decimal TotalIncomes => TransactionsList
+        .Where(t => t.Type == TypeTransaction.Income)
+        .Sum(t => t.Value);
+
+    public string TotalIncomesFormatted => $"{TotalIncomes:C2}";
+
+    private decimal TotalOutcomes => TransactionsList
+        .Where(t => t.Type == TypeTransaction.Outcome)
+        .Sum(t => t.Value);
+
+    public string TotalOutcomesFormatted => $"{TotalOutcomes:C2}";
+
+    private decimal Balance => TotalIncomes - TotalOutcomes;
+
+    public string BalanceFormatted => $"{Balance:C2}";
 
     public TransactionsViewModel()
     {
         using var db = new ContextDB();
         db.Database.EnsureCreated();
         TransactionsList = new ObservableCollection<Transaction>([.. db.Transactions]);
+        TransactionsList.CollectionChanged += (_, __) =>
+        {
+            OnPropertyChanged(nameof(TotalOutcomesFormatted));
+            OnPropertyChanged(nameof(TotalIncomesFormatted));
+            OnPropertyChanged(nameof(BalanceFormatted));
+        };
     }
+
+    protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    
 }
