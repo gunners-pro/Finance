d192718984c594b429d225a9c8c82884dec7bedb
Fabricyo Barreto <bass.barreto@gmail.com>
2025-06-09 22:55:34 -0300

    feat: add Graphics

--- Finance/ViewModels/TransactionsViewModel.cs
+++ Finance/ViewModels/TransactionsViewModel.cs
@@ -1,5 +1,9 @@
 ﻿using Finance.Data;
 using Finance.Models;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+using SkiaSharp;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
@@ -27,6 +31,12 @@ public class TransactionsViewModel : INotifyPropertyChanged
 
     public string BalanceFormatted => $"{Balance:C2}";
 
+    //===================== gráficos
+    public ISeries[] Series { get; private set; } = [];
+    public Axis[] XAxes { get; set; } = [];
+
+    // ============================
+
     public TransactionsViewModel()
     {
         using var db = new ContextDB();
@@ -37,8 +47,54 @@ public class TransactionsViewModel : INotifyPropertyChanged
             OnPropertyChanged(nameof(TotalOutcomesFormatted));
             OnPropertyChanged(nameof(TotalIncomesFormatted));
             OnPropertyChanged(nameof(BalanceFormatted));
+            OnPropertyChanged(nameof(Series));
+            BuildSeries();
         };
+
+        BuildSeries();
+
     }
+
+    private void BuildSeries()
+    {
+        var grouped = TransactionsList.GroupBy(t => t.Date.Date).OrderBy(g => g.Key).ToList();
+
+        var labels = grouped.Select(g => g.Key.ToString("dd/MM")).ToArray();
+
+        var entradas = grouped.Select(g => g.Where(t => t.Type == TypeTransaction.Income).Sum(t => t.Value)).ToArray();
+
+        var saidas = grouped.Select(g => g.Where(t => t.Type == TypeTransaction.Outcome).Sum(t => t.Value)).ToArray();
+
+        Series =
+        [
+        new ColumnSeries<decimal>
+        {
+            Values = entradas,
+            Name = "Entradas",            
+            Fill = new SolidColorPaint(SKColors.Green),
+            MaxBarWidth = 30
+        },
+        new ColumnSeries<decimal>
+        {
+            Values = saidas,
+            Name = "Saídas",
+            Fill = new SolidColorPaint(SKColors.Red),
+            MaxBarWidth = 30
+        }
+        ];
+
+        XAxes = [
+            new Axis{
+                Labels = labels,
+                Name = "Data",
+            }
+        ];
+
+        OnPropertyChanged(nameof(Series));
+        OnPropertyChanged(nameof(XAxes));
+
+    }
+
 
     protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
     {
